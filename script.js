234527e069771e368a9a86c94c015ec4691073d5
Elliott Silverman <elliotts@ualberta.ca>
2021-02-02 15:44:00 -0700

    Creaded the Add Book event listener and toogleForm functionality for it.

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function Book(title, author, pages, status) {
     this.pages = pages;
     this. status = status;
 }
+
+//ADD BOOK BUTTON
+const addBookBtn = document.querySelector(".addBook");
+console.log(addBookBtn);
+addBookBtn.addEventListener("click", toogleForm);
+
+function toogleForm() {
+    let bookForm = document.querySelector(".bookForm");
+    console.log(bookForm);
+    if (bookForm.style.display === "none") {
+        bookForm.style.display = "block";
+    } else {
+        bookForm.style.display = "none";
+    }
+}
