5791bf8cb41a7ebbb0506fa25c10b3038839187c
Elliott Silverman <elliotts@ualberta.ca>
2021-02-06 19:20:15 -0700

    Writing the addBookToLibrary function.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let myLibrary = [];
 
+//BOOK CONSTRUCTOR
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this. status = status;
+}
+
+//ADD BOOK TO LIBRARY ARRAY
+function addBookToLibrary(); {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const hasRead = document.getElementById("read");
+
+    const newBook = Object.create(Book);
+
+    newBook.title = title.innerText;
+    newBook.author = author.innerText;
+    newBook.pages = pages.innerText;
+    newBook.status = hasRead.checked;
+
 }
 
 // MODAL FUNCTIONALITY
